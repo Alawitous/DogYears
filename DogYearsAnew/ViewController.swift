fac63096ca5e5ecd53fff682e236e0c8660b119c
Marcus Dunlap <marcusdunlap@Marcuss-MacBook-Pro-7.local>
2015-01-13 00:04:42 -0500

    rev number one

--- DogYearsAnew/ViewController.swift
+++ DogYearsAnew/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogAgeConverted: UILabel!
+    
+    @IBOutlet weak var enteredHumanYears: UITextField!
+    
+    @IBOutlet weak var convertToDogYearsButton: UIButton!
+    
+    @IBOutlet weak var realDogYearConversion: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func convertToDogYearsButton(sender: UIButton) {
+        let humanTextField = enteredHumanYears.text.toInt()!
+        dogAgeConverted.text = "\(humanTextField * 7)" + " Dog Years"
+        dogAgeConverted.hidden = false
+        dogAgeConverted.textColor = UIColor.redColor()
+        enteredHumanYears.resignFirstResponder()
+    }
+    
+    @IBAction func realDogYearConversion(sender: UIButton) {
+        let humanTextField = Double((enteredHumanYears.text as NSString).doubleValue)
+        if humanTextField > 2 {
+            dogAgeConverted.text = "\((2 * 10.5) + ((humanTextField - 2) * 4))" + " Real Dog Years"
+        }
+        else {
+            dogAgeConverted.text = "\(humanTextField * 10.5)" + " Dog Years"
+        }
+        dogAgeConverted.hidden = false
+        dogAgeConverted.textColor = UIColor.redColor()
+        enteredHumanYears.resignFirstResponder()
 
+    }
+        
+        
 }
+
 
